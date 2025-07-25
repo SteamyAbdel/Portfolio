2a279e19b8439d529ac450319635198b841b2ea5
Abdelali <steamyabdel@gmail.com>
2025-07-25 13:44:55 +0200

    chore: add @vercel/speed-insights dependency to enhance performance monitoring

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import StarsCanvas from "@/components/main/StarsBackground";
 import Navbar from "@/components/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,6 +34,7 @@ export default function RootLayout({
         <StarsCanvas />
         <Navbar />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
