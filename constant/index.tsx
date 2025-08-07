ef14183d3193e18ec6ce7b2e89f48742389ed68f
Abdelali <steamyabdel@gmail.com>
2025-08-07 20:52:00 +0200

    refactor projects to use mapped data

--- constant/index.tsx
+++ constant/index.tsx
@@ -15,3 +15,34 @@ export const Social_Icons = [
     alt: "LinkedIn",
   },
 ];
+
+export const projects = [
+  {
+    title: "StudentScore (C1, C4, C5)",
+    description:
+      "Application de gestion de notes avec système de récompenses faite sous Androïd avec Android Studio.",
+    image: "/projets/StudentScore.png",
+    url: "/projets/studentscore",
+  },
+  {
+    title: "Chabis (C1, C4, C5)",
+    description:
+      "Cette application doit permettre de gérer l'ensemble des chèvres de la société CHABIS, de la plus petite à la plus ancienne.",
+    image: "/projets/php.png",
+    url: "/projets/chabis",
+  },
+  {
+    title: "CHL (C1, C2, C4, C5, C6)",
+    description:
+      "Ce projet vise à créer une interface intuitive où les agents pourront facilement proposer, voter et visualiser les disponibilités, tout en assurant la sécurité et la confidentialité des données manipulées.",
+    image: "/projets/chl.png",
+    url: "/projets/chl",
+  },
+  {
+    title: "BLUE COM (C1, C2, C4, C5, C6)",
+    description:
+      "Ce projet a été la refonte du site vitrine de l'agence BlueCom qui comprend un back-end en Symfony et l'installation de API Platform et le front avec du NextJS.",
+    image: "/projets/blue-com.svg",
+    url: "/projets/bluecom",
+  },
+];
